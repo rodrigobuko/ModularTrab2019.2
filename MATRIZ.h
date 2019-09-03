109969a54f572ad91e65e6392f78abf45ec9f032
Pedro Bernardes <pedrobernar10@gmail.com>
2019-09-03 12:02:10 -0300

    oi2

--- MATRIZ.h
+++ MATRIZ.h
@@ -1,4 +1,10 @@
 
+/***********************************************************************
+*
+*  $TC Tipo de dados: Ponteiro para matriz
+*
+*
+***********************************************************************/
 
 typedef tpMatriz pMatriz;
 
@@ -39,25 +45,35 @@ typedef tpMatriz pMatriz;
 *
 *  $FC Função: MAT Criar matriz
 *
+*  $EP Parâmetros
+*  	  $P tpMatriz - ponteiro que apontará para a matriz criada.
+*  	  $P PonteirosDosValores - aponta para uma lista contendo os ponteiros para os valores que cada célula deverá ter.
+*  	  $P dimensao - a matriz criada será quadrada com esse valor de linhas e colunas. Deve ser maior que zero.
+*
 *  $ED Descrição da função
-*     Cria uma nova matriz vazia.
-*     Caso já exista uma matriz, esta será destruída.
+*     Cria uma nova matriz vazia e seus nós. Preenche os valores e as ligações dos nós.
+*     Caso já exista uma matriz, retorna um erro.
 *
 *  $FV Valor retornado
 *     MAT_CondRetOK
 *     MAT_CondRetFaltouMemoria
+*	  MAT_CondRetDimensaoInvalida
+*	  MAT_CondRetMatrizJaExiste
 *
 ***********************************************************************/
 
-   MAT_tpCondRet MAT_CriarMatriz( tpMatriz* pMatriz ) ;
+   MAT_tpCondRet MAT_CriarMatriz( tpMatriz* pMatriz, void** PonteirosDosValores, int dimensao ) ;
 
 
 /***********************************************************************
 *
 *  $FC Função: MAT Destruir matriz
 *
+*  $EP Parâmetros
+*  	  $P tpMatriz - ponteiro que aponta para a matriz sobre a qual se deseja operar.
+*
 *  $ED Descrição da função
-*     Destrói o corpo e a cabeça da matriz, anulando a matriz corrente.
+*     Destrói o corpo e a cabeça da matriz, anulando o ponteiro passado como parâmetro.
 *     Faz nada caso a matriz corrente não exista.
 *
 ***********************************************************************/
@@ -69,10 +85,14 @@ typedef tpMatriz pMatriz;
 *
 *  $FC Função: MAT Ir para direção especificada
 *
+*  $EP Parâmetros
+*  	  $P tpMatriz - ponteiro que aponta para a matriz sobre a qual se deseja operar.
+*     $P Direcao - caracteres que indicam para qual dos nós vizinhos pCorr irá.
+*		pode ser "n", "s", "e", "o", "ne", "se", "so" ou "no"
+
 *  $FV Valor retornado
 *     MAT_CondRetOK
-*     MAT_CondRetArvoreNaoExiste
-*     MAT_CondRetArvoreVazia
+*     MAT_CondRetMatrizNaoExiste
 *     MAT_CondRetDirecaoVazia
 *
 ***********************************************************************/
@@ -85,13 +105,13 @@ typedef tpMatriz pMatriz;
 *  $FC Função: MAT Obter valor corrente
 *
 *  $EP Parâmetros
+*  	  $P tpMatriz - ponteiro que aponta para a matriz sobre a qual se deseja operar.
 *     $P ValorParm - é o parâmetro que receberá o valor contido no nó.
 *                    Este parâmetro é passado por referência.
 *
 *  $FV Valor retornado
 *     MAT_CondRetOK
-*     MAT_CondRetArvoreNaoExiste
-*     MAT_CondRetArvoreVazia
+*     MAT_CondRetMatrizNaoExiste
 *
 ***********************************************************************/
 
